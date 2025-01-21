b8ff5337bf2b16c469cf032755430250aaaff71a
DianaGakuya <deegakuya@gmail.com>
2025-01-21 17:16:56 +0300

    Developed HomeScreen layout and styled Navbar with logo and user avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './HomeScreen'; // Import HomeScreen component
 
 function App() {
   return (
     <div className="app">
-    
+     
+      <HomeScreen /> {/* Using the HomeScreen component */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css';
+
+
+  {/*<img src="/fiFilmsafilogo1.png" alt="Filmsafi Logo" />*/}
+  function Nav() {
+    return (
+      <div className="nav">
+        
+        <div className="nav_contents">
+        <img src="/filmsafilogo1.png" alt="Filmsafi Logo" />
+           
+        <h1>This is the nav </h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Nav; 
+  
